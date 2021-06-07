e9777aba9b4ed407ebb4705ef7701fddb1018caa
Rmihaja <50056520+Rmihaja@users.noreply.github.com>
2021-06-07 17:53:20 +0200

    ✨ Add data binding across component
    Spread posts value to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../assets/Post';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngBlog';
+  
+  posts!: Post[];
+
+  constructor() { }
+  
+  ngOnInit() {
+
+    this.posts = [
+      new Post('Mon premier post', 'Hello'),
+      new Post('Mon deuxième post', 'blablabla'),
+      new Post('Encore un post', 'Lorem ipsum dolor sit amet, consectetur adpisicing elit, sed do elusmod tempr incidunt ut labore et dolore et do etc')
+    ]
+
+  }
+
 }
